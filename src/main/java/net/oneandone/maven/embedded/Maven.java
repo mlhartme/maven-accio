889deba65bee1a70dc55f0faa3b8014649e0ff20
Michael Hartmeier <mlhartme.users.noreply@github.com>
2023-07-26 14:30:57 +0200

    docs

--- src/main/java/net/oneandone/maven/embedded/Maven.java
+++ src/main/java/net/oneandone/maven/embedded/Maven.java
@@ -93,6 +93,8 @@ import java.util.Properties;
  * * does not load Maven extensions, neither core nor plugin nor build extensions
  *   (Maven does so in DefaultMaven
  *   https://github.com/apache/maven/blob/21122926829f1ead511c958d89bd2f672198ae9f/maven-core/src/main/java/org/apache/maven/DefaultMaven.java#L327C5-L327C5)
+ * * does not setup/use EventSpyDispatcher/EventSpy, so the respective events are not fired/processed (Maven uses this for plugin validation)
+ * * does not setup/use ExecutionEventCatapult/ExecutionListener (Maven does use them for console/log output)
  */
 public class Maven {
     public static Maven withSettings(World world) throws IOException {
