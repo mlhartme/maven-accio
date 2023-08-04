b45b1fa52907d4c2ffd0d5c92daed615f6048467
Michael Hartmeier <mlhartme.users.noreply@github.com>
2023-08-04 09:42:26 +0200

    configure RejectingMavenPluginManager with properties

--- src/main/java/net/oneandone/maven/accio/RejectingMavenPluginManager.java
+++ src/main/java/net/oneandone/maven/accio/RejectingMavenPluginManager.java
@@ -14,18 +14,41 @@ import java.util.List;
 
 @Component(role = MavenPluginManager.class)
 public class RejectingMavenPluginManager extends DefaultMavenPluginManager {
-    private List<String> allowed = new ArrayList<>();
+    private final String logPrefix;
+
+    private List<String> allowUrls;
+
+    public RejectingMavenPluginManager() {
+        super();
+        this.logPrefix = getClass().getSimpleName() + ":";
+        this.allowUrls = new ArrayList<>();
+        addAllowProperty();
+        // TODO
+        System.out.println(logPrefix + "initialized, allow " + allowUrls);
+    }
+
+    public void addAllowProperty() {
+        String property = System.getProperty(getClass().getName() + ":allow");
+        if (property != null) {
+            for (String entry : property.split(",")) {
+                if (!entry.isBlank()) {
+                    allowUrls.add(entry.trim());
+                }
+            }
+        }
+    }
 
     public void allow(String url) {
-        allowed.add(url);
+        allowUrls.add(url);
     }
+
     @Override
     public ExtensionRealmCache.CacheRecord setupExtensionsRealm(
             MavenProject project, Plugin plugin, RepositorySystemSession session) throws PluginManagerException {
         // getPluginRepositories normally yields the effective pom repositories AND the repositories from the effective settings!
         // but at this point in parsing, we don't have the effective pom yet. So the DefaultMavenPluginManager uses getRemotePluginRepositories
         for (var repo : project.getRemotePluginRepositories()) {
-            if (!allowed.contains(repo.getUrl())) {
+            if (!allowUrls.contains(repo.getUrl())) {
                 throw new IllegalArgumentException("repository url rejected: " + repo.getUrl());
             }
         }
